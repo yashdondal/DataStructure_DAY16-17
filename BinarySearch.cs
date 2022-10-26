1fdccc280def3f192c5160ee2ef789d00cbf93d3
yashdondal <yashdondal21@gmail.com>
2022-10-26 17:31:46 +0530

    binary search

---
+++ BinarySearch.cs
@@ -0,0 +1,25 @@
+﻿namespace AlgoritmProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your choice:\n1.Insertion Sort\n2.Bubble Sort\n3.Binary search\n");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("---------------------------------");
+            switch (choice)
+            {
+                
+                case 1:
+                    const string FILE_PATH = @"C:\Users\yash dondal\Desktop\208\DataStructure_DAY16,17\New.txt\";
+                    BinarySearch search = new BinarySearch();
+                    search.Search(FILE_PATH);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmProblem
+{
+    public class BinarySearch
+    {
+        public void Search(string filePath)
+        {
+
+            string data = File.ReadAllText(filePath);
+            string[] words = data.Split(",");
+            Console.WriteLine("---------------------------\n");
+            Console.WriteLine("Enter Words To Find From WordList :");
+            string search = Console.ReadLine();
+            foreach (var item in words)
+            {
+                if (search.ToLower().Equals(item.ToLower()))
+                {
+                    Console.WriteLine("Word Found");
+                    Console.WriteLine("---------------------------\n");
+                    return;
+                }
+            }
+            Console.WriteLine("Word Not Found");
+            Console.WriteLine("---------------------------\n");
+        }
+    }
+}
